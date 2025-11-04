ae422710d7b7fcacb10aedefaa4b2a7ecc621ad4
Himesh Dua <himeshdua22@gmail.com>
2025-11-04 08:08:45 +0500

    refactor: update layout and manifest files for improved PWA support and icon management

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import type { Metadata, Viewport } from 'next';
-import { Geist } from 'next/font/google';
-import { ThemeProvider } from 'next-themes';
+import type {Metadata, Viewport} from 'next';
+import {Geist} from 'next/font/google';
+import {ThemeProvider} from 'next-themes';
 import './globals.css';
 import './tiptap.scss';
 import PageShell from '@/components/layout/pageShell';
@@ -17,15 +17,14 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const viewport: Viewport = {
   maximumScale: 1,
-  userScalable: false
-}
+  userScalable: false,
+};
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
   title: 'LibreBooks',
   description: 'FreeBooks Library – Read, explore, and manage your favorite books online.',
   applicationName: 'LibreBooks',
-  manifest: '/site.webmanifest',
   // icons: {
   //   icon: [
   //     { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
@@ -34,103 +33,60 @@ export const metadata: Metadata = {
   //   ],
   //   shortcut: '/favicon.ico',
   //   apple: [
-  //     { url: '/apple-touch-icon.png', sizes: '180x180' },
+  //     { },
   //   ],
   // },
   themeColor: '#0f172a',
 
   icons: {
     icon: [
-      { url: "/android/android-launchericon-192-192.png", sizes: "192x192", type: "image/png" },
-      { url: "/android/android-launchericon-512-512.png", sizes: "512x512", type: "image/png" }
+      {url: '/android/android-launchericon-192-192.png', sizes: '192x192', type: 'image/png'},
+      {url: '/android/android-launchericon-512-512.png', sizes: '512x512', type: 'image/png'},
     ],
     shortcut: '/favicon.ico',
     apple: [
-      { url: "/ios/180.png" },
-      { url: "/ios/152.png" },
-      { url: "/ios/167.png" }
+      {url: '/apple-icon.png', sizes: '180x180'},
+      {url: '/ios/180.png'},
+      {url: '/ios/152.png'},
+      {url: '/ios/167.png'},
     ],
   },
   appleWebApp: {
     capable: true,
-    title: "LibreBooks",
-    statusBarStyle: "black-translucent",
+    title: 'LibreBooks',
+    statusBarStyle: 'black-translucent',
     startupImage: [
-      {
-        url: "/ios/2048x2732.png",
-        media:
-          "(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",
-      },
-      {
-        url: "/ios/2732x2048.png",
-        media:
-          "(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)",
-      },
-      {
-        url: "/ios/1668x2388.png",
-        media:
-          "(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",
-      },
-      {
-        url: "/ios/2388x1668.png",
-        media:
-          "(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)",
-      },
-      {
-        url: "/ios/1536x2048.png",
-        media:
-          "(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",
-      },
-      {
-        url: "/ios/2048x1536.png",
-        media:
-          "(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)",
-      },
-      {
-        url: "/ios/1242x2688.png",
-        media:
-          "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)",
-      },
-      {
-        url: "/ios/2688x1242.png",
-        media:
-          "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)",
-      },
-      {
-        url: "/ios/1125x2436.png",
-        media:
-          "(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)",
-      },
-      {
-        url: "/ios/2436x1125.png",
-        media:
-          "(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)",
-      },
-      {
-        url: "/ios/828x1792.png",
-        media:
-          "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",
-      },
-      {
-        url: "/ios/1792x828.png",
-        media:
-          "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)",
-      },
-      {
-        url: "/ios/750x1334.png",
-        media:
-          "(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",
-      },
-      {
-        url: "/ios/1334x750.png",
-        media:
-          "(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)",
-      },
+      {url: '/ios/16.png', media: '(max-resolution: 16x16)'},
+      {url: '/ios/20.png', media: '(max-resolution: 20x20)'},
+      {url: '/ios/29.png', media: '(max-resolution: 29x29)'},
+      {url: '/ios/32.png', media: '(max-resolution: 32x32)'},
+      {url: '/ios/40.png', media: '(max-resolution: 40x40)'},
+      {url: '/ios/50.png', media: '(max-resolution: 50x50)'},
+      {url: '/ios/57.png', media: '(max-resolution: 57x57)'},
+      {url: '/ios/58.png', media: '(max-resolution: 58x58)'},
+      {url: '/ios/60.png', media: '(max-resolution: 60x60)'},
+      {url: '/ios/64.png', media: '(max-resolution: 64x64)'},
+      {url: '/ios/72.png', media: '(max-resolution: 72x72)'},
+      {url: '/ios/76.png', media: '(max-resolution: 76x76)'},
+      {url: '/ios/80.png', media: '(max-resolution: 80x80)'},
+      {url: '/ios/87.png', media: '(max-resolution: 87x87)'},
+      {url: '/ios/100.png', media: '(max-resolution: 100x100)'},
+      {url: '/ios/114.png', media: '(max-resolution: 114x114)'},
+      {url: '/ios/120.png', media: '(max-resolution: 120x120)'},
+      {url: '/ios/128.png', media: '(max-resolution: 128x128)'},
+      {url: '/ios/144.png', media: '(max-resolution: 144x144)'},
+      {url: '/ios/152.png', media: '(max-resolution: 152x152)'},
+      {url: '/ios/167.png', media: '(max-resolution: 167x167)'},
+      {url: '/ios/180.png', media: '(max-resolution: 180x180)'},
+      {url: '/ios/192.png', media: '(max-resolution: 192x192)'},
+      {url: '/ios/256.png', media: '(max-resolution: 256x256)'},
+      {url: '/ios/512.png', media: '(max-resolution: 512x512)'},
+      {url: '/ios/1024.png', media: '(max-resolution: 1024x1024)'},
     ],
   },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.className} antialiased`}>
