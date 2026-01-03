26002ce1a008f6a3759c8ec554e8269a59a7101d
Himesh Dua <himeshdua22@gmail.com>
2026-01-03 17:53:36 +0500

    some changes

--- lib/getUserByInfo.ts
+++ lib/getUserByInfo.ts
@@ -6,6 +6,7 @@ export const getUserByInfo = async () => {
   const user = data?.user;
   if (!user) return {user: null};
   if (error) console.error(error.message);
+  console.log('i got hit', user);
   // cacheTag('user', user.id);
   // cacheLife('hours');
   return {user: user};
