00a78adc8c752d70ddf0f6377539d8ee51e8c1cd
Himesh Dua <himeshdua22@gmail.com>
2026-01-03 21:14:19 +0500

    added api instead of client page

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -11,7 +11,13 @@ export function LoginForm() {
       toast.info('Redirecting to Google sign-in...');
       const {error} = await supabase.auth.signInWithOAuth({
         provider: 'google',
-        options: {redirectTo: `${window.location.origin}/auth/callback`},
+        options: {
+          redirectTo: `${window.location.origin}/api/auth/callback`,
+          queryParams: {
+            access_type: 'offline',
+            prompt: 'consent',
+          },
+        },
       });
 
       if (error) {
--- components/sign-up-form.tsx
+++ components/sign-up-form.tsx
@@ -10,7 +10,13 @@ export function SignUpForm() {
     try {
       const {error} = await supabase.auth.signInWithOAuth({
         provider: 'google',
-        options: {redirectTo: `${window.location.origin}/auth/callback`},
+        options: {
+          redirectTo: `${window.location.origin}/api/auth/callback`,
+          queryParams: {
+            access_type: 'offline',
+            prompt: 'consent',
+          },
+        },
       });
 
       if (error) {
