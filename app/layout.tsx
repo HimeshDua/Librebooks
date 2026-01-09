bb8f36678ad2ecc413998c388f543e80f97c356d
Himesh Dua <himeshdua22@gmail.com>
2026-01-09 23:45:33 +0500

    Add Google site verification meta tag
    Added Google site verification meta tag for SEO.

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,6 +71,10 @@ export default async function RootLayout({children}: {children: React.ReactNode}
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.className} antialiased reader max-w-screen min-h-[94vh]`}>
+          <meta
+          name="google-site-verification"
+          content="ZqN_pVPqcbynnIC9mI9y7Zk3IDzxfkwuP9hapELyTuU"
+        />
         <script src="https://accounts.google.com/gsi/client" async></script>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           <NextTopLoader showSpinner={false} />
