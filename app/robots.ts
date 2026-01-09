a5ae6b48f7230d22bc33afc6d638086c2a22573b
Himesh Dua <himeshdua22@gmail.com>
2026-01-10 00:13:02 +0500

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,11 +1,11 @@
-import type { MetadataRoute } from 'next'
- 
+import { MetadataRoute } from 'next';
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: '*',
+      allow: '/',
     },
-    sitemap: 'https://librebooks.vercel.app',
-  }
+    sitemap: 'https://librebooks.vercel.app/sitemap.xml/',
+  };
 }
