760f61fd1532945a834869c05278b145403e29e5
Himesh Dua <himeshdua22@gmail.com>
2026-02-13 22:29:34 +0500

    small bug fix in library book skeleton grid col

--- components/skeletons/book-display.tsx
+++ components/skeletons/book-display.tsx
@@ -1,6 +1,6 @@
 export function BookDisplaySkeleton() {
   return (
-    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
+    <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
       {Array.from({length: 12}).map((_, i) => (
         <li
           key={i}
